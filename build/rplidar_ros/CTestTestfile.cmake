caee248f2f80e5f1352dd949b5df9bb554527344
Lucas Lagoeiro <llagoeiro@outlook.com.br>
2024-03-19 22:43:14 -0300

    First commit - Creating ws for the project

---
+++ build/rplidar_ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wall-e/Desktop/ws_project/src/rplidar_ros
+# Build directory: /home/wall-e/Desktop/ws_project/build/rplidar_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
