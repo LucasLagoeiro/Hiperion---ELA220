cd737b90705e2524e5abc84be6bf9156ea191529
LucasLagoeiro <llagoeiro@outlook.com.br>
2024-03-22 11:26:54 -0300

    The URDF car is spawning in gazebo world (need to fix the rotations)

--- build/mpu6050driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wall-e/Desktop/ws_project/src/ros2_mpu6050_driver
-# Build directory: /home/wall-e/Desktop/ws_project/build/mpu6050driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
